e7029d5a178cb9ba200249441dca76cff7d93857
CedricLapi <134283826+CedricLapi@users.noreply.github.com>
2024-07-26 02:28:20 +0200

    Add files via upload

---
+++ PacktLearnC/Chapter1_HelloWorld/hello2.c
@@ -0,0 +1,20 @@
+/* 
+* hello2.c
+* My first C program with comments.
+* by Cedric Lapi
+* created 2024/07/24
+ */
+
+
+
+#include <stdio.h>
+
+int main(void)
+
+
+{
+    printf( "Hello, world!\n" );
+    return 0;
+}
+
+/* eof */
---
+++ PacktLearnC/Chapter1_HelloWorld/printingExcapeSequences.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main( void )
+
+{
+
+    printf( "Hello, world without a new line" );
+    printf( "Hello, world with a new line\n");
+    printf( "A string with \"quoted text\" inside of it\n\n" );
+    printf( "Tabbed\tColumn\tHeadings\n");
+    printf( "The \tquick\tBrown\n");
+    printf( "fox \tjumps\tOver\n");
+    printf( "the \tlazy\tdog. \n\n");
+    printf( "A line of text that\nspans three lines\nand completes the line\n\n" );
+    
+}
